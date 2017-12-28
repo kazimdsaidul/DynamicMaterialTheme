33473310138c34a9e48e5e187a861dacac0a60c2
Kazi Md. Saidul <kazis@springrainit.com>
2017-12-28 16:42:48 +0600

    Initial commit

---
+++ app/src/main/java/com/dynamictheme/MainActivity.java
@@ -0,0 +1,240 @@
+package com.dynamictheme;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
+import android.support.v7.widget.Toolbar;
+import android.transition.Explode;
+import android.transition.Transition;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.backends.pipeline.PipelineDraweeController;
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.common.ResizeOptions;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
+import com.squareup.picasso.Picasso;
+
+import static com.facebook.drawee.backends.pipeline.Fresco.*;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private LinearLayout layout_green, layout_purple;
+    private SwitchCompat switch_dark;
+    private boolean isDarkTheme;
+    private static final String Theme_Current = "ThemeCurrent";
+    private static final String Dark_Theme = "DarkTheme";
+    private SimpleDraweeView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        initialize(this);
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+
+            //set the transition
+            Transition ts = new Explode();
+            ts.setDuration(5000);
+            getWindow().setEnterTransition(ts);
+            getWindow().setExitTransition(ts);
+        }
+
+        super.onCreate(savedInstanceState);
+
+        setAppTheme();
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TaskStackBuilder.create(MainActivity.this)
+                        .addNextIntent(new Intent(MainActivity.this, MainActivity.class))
+                        .addNextIntent(getIntent())
+                        .startActivities();
+
+            }
+        });
+
+
+
+        layout_green = (LinearLayout) findViewById(R.id.Layout_green);
+        layout_purple = (LinearLayout) findViewById(R.id.Layout_purple);
+        imageView = (SimpleDraweeView) findViewById(R.id.imageView);
+
+
+
+
+
+        layout_green.setOnClickListener(this);
+        layout_purple.setOnClickListener(this);
+
+        chanageBackgroundImage();
+
+
+    }
+
+
+    private void chanageBackgroundImage() {
+
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int width = displayMetrics.widthPixels;
+        int height = displayMetrics.heightPixels;
+
+        if (!MainController.preferenceGetString(Theme_Current, "").equals("")) {
+            if (MainController.preferenceGetString(Theme_Current, "").equals("Green")) {
+
+                Picasso.with(MainActivity.this)                   //Activity context
+                        .load(R.drawable.bg_green)
+                        .resize(width, height)
+                        .onlyScaleDown() // the image will only be resized if it's bigger than 6000x2000 pixels.
+                        .into(imageView);
+
+            } else if (MainController.preferenceGetString(Theme_Current, "").equals("Purple")) {
+
+                Picasso.with(MainActivity.this)                   //Activity context
+                        .load(R.drawable.bg_purple)
+                        .resize(width, height)
+                        .onlyScaleDown() // the image will only be resized if it's bigger than 6000x2000 pixels.
+                        .into(imageView);
+            }
+        } else {
+            Picasso.with(MainActivity.this)                   //Activity context
+                    .load(R.drawable.bg_green)
+                    .resize(width, height)
+                    .onlyScaleDown() // the image will only be resized if it's bigger than 6000x2000 pixels.
+                    .into(imageView);
+        }
+
+
+
+
+    }
+
+
+    private void setAppTheme() {
+
+        if (!MainController.preferenceGetString(Theme_Current, "").equals("")) {
+            if (MainController.preferenceGetString(Theme_Current, "").equals("Green")) {
+                setTheme(R.style.ThemeApp_Green);
+            } else if (MainController.preferenceGetString(Theme_Current, "").equals("Purple")) {
+                setTheme(R.style.ThemeApp_Purple);
+            }
+        } else {
+            setTheme(R.style.ThemeApp_Green);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.Layout_green:
+
+
+                    MainController.preferencePutString(Theme_Current, "Green");
+
+
+                break;
+
+            case R.id.Layout_purple:
+
+
+                    MainController.preferencePutString(Theme_Current, "Purple");
+
+
+                /*TaskStackBuilder.create(this)
+                        .addNextIntent(new Intent(this, Listactivity.class))
+                        .addNextIntent(getIntent())
+                        .startActivities();*/
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast.makeText(MainActivity.this, "Action clicked", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+}
